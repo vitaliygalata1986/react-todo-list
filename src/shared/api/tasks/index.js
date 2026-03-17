f8e0f7b830aa463b2f17f29211f7b6e6c11992fb
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2026-03-18 00:11:31 +0200

    Added comment to vite.config

--- src/shared/api/tasks/index.js
+++ src/shared/api/tasks/index.js
@@ -2,8 +2,31 @@ import localAPI from './local';
 
 import serverAPI from './server';
 
-const isLocal = import.meta.env.VITE_STATIC_BACKEND === 'true';
+const isLocal = import.meta.env.VITE_STATIC_BACKEND === 'true'
 
-const tasksAPI = isLocal ? localAPI : serverAPI;
+const tasksAPI = isLocal ? localAPI : serverAPI
 
-export default tasksAPI;
+export default tasksAPI
+
+
+/*
+    Если ты запускаешь локально npm run dev, это влияет на mode = 'development', но isLocal зависит не от этого, а от значения VITE_STATIC_BACKEND.
+    То есть:
+        const isLocal = import.meta.env.VITE_STATIC_BACKEND === 'true'
+        будет true только если Vite подхватил: VITE_STATIC_BACKEND=true
+
+    При локальном запуске Vite обычно читает:
+    •.env
+    •.env.local
+    •.env.development
+    •.env.development.local
+
+    При production build:
+    •.env
+    •.env.production
+    •.env.production.local
+
+    Итого: локальный запуск сам по себе не делает localAPI = true. Это будет только если в dev-env файлах у тебя VITE_STATIC_BACKEND=true.
+    Если хочешь, могу быстро посмотреть в проекте, из какого .env у тебя это сейчас берётся.
+
+* */
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import {fileURLToPath, URL} from 'node:url';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // в mode будет строка с режимом сборки - dev/prod
-  const isProd = mode === 'production';
-  return {
-    base: isProd ? '/react-todo-list/' : '/',
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)), // заменяем src на import.meta.url
-      },
-    },
-  };
+export default defineConfig(({mode}) => {
+    // в mode будет строка с режимом сборки - dev/prod
+    const isProd = mode === 'production';
+    return {
+        base: isProd ? '/react-todo-list/' : '/',
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)), // заменяем src на import.meta.url
+            },
+        },
+    };
 });
 
 // alias настраивается на уровне сборщика проектов
+
+/*
+  Как Vite понимает режим:
+    • npm run dev / vite -> mode === 'development'
+    • npm run build / vite build -> mode === 'production'
+    • можно явно задать: vite build --mode staging -> mode === 'staging'
+    Поэтому строка
+      const isProd = mode === 'production';
+      означает: "если сборка запущена в production-режиме, isProd будет true".
+* */
